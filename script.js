68377ea5fd94847f09c6551ce84004cf5ca56022
chheangg <balldremz@gmail.com>
2021-12-18 20:45:43 +0700

    Ensure full functionality
    Some bugs such as the value of '0' not displaying after a dot in a float
    int will now appear. Full functionality is ensured and some UI changes
    are made! (Such as the footer).

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let num1 = '0';
 let num2 = '0';
+let temp = '';
 let sign = '';
-let btn = document.querySelectorAll('button');
-let displayField = document.querySelector('.field');
-let displaySign = document.querySelector('.display-sign');
-let posnegbtn = document.querySelector('.posneg');
+const btn = document.querySelectorAll('button');
+const displayField = document.querySelector('.field');
+const displaySign = document.querySelector('.display-sign');
+const posnegbtn = document.querySelector('.posneg');
+const sliceNum = document.querySelector('.slice-num');
+
 
 btn.forEach((button) => {
     button.addEventListener('click', (value) => {
@@ -16,6 +19,10 @@ posnegbtn.addEventListener('click', () => {
     displayField.textContent = -parseInt(displayField.textContent); 
 })
 
+sliceNum.addEventListener('click', () => {
+    displayField.textContent = displayField.textContent.slice(displayField.textContent.length)
+})
+
 const add = function(x, y) {
     return +x + +y;
 }
@@ -25,7 +32,7 @@ const subtract = function(x, y) {
 }
 
 const multiply = function(x, y) {
-    return ((x * 10)/10 ) * y;
+    return (((x * 10) * y)/10 );
 };
 
 const divide = function (x, y) {
@@ -61,49 +68,66 @@ function roundNum(num) {
     }
     return num;
 }
-function calcValue(value) {
-    value = roundNum(value);
-    if ( value == '+' || value == '-' || value == '/' || value == '*' || value == '%') {
-        displaySign.textContent = value;
-        sign = value;
-        if ( (num1 != '' && num1 != '0') && (num2 != '' && num2 != '0') ) {
-            num1 = operate(num1, num2, value);
-            displayField.textContent = num1;
-            sign = '';
-            num2 = '';
-        } 
-    } else if ( displayField.textContent.includes('.') && value == '.' ) {
 
-    } else if ( value == 'AC' ) {
-        num1 = '';
+/* Check if there has been an operation */
+function checkPrevious(value) {
+    if ( temp == true && typeof +value == 'number') {
+        displayField.textContent = '';
         num2 = '';
         sign = '';
-        displayField.textContent = '';
-        displaySign.textContent = '';
-    } else if ( value == '=') {
-        if ((num1 != '' && num1 != '0') && (num2 != '' && num2 != '0')) {
-            displaySign.textContent = ''
-            num1 = operate(+num1, +num2, sign);
-            displayField.textContent = num1;
-            sign = '';
+        temp = false;
+    }
+}
+
+function calcValue(value) {
+    value = roundNum(value);
+        if ( value == '+' || value == '-' || value == '/' || value == '*' || value == '%') {
+            displaySign.textContent = value;
+            sign = value;
+            if ( (num1 != '' && num1 != '0') && (num2 != '' && num2 != '0') ) {
+                num1 = operate(num1, num2, value);
+                displayField.textContent = num1;
+                num2 = '';
+                temp = true;
+            } 
+        } else if ( value == '.' ) {
+            if ( displayField.textContent.includes('.') ) {
+            return;
+            } else {
+                displayField.textContent += value;  
+            }
+        } else if ( value == 'AC' ) {
+            num1 = '';
             num2 = '';
-        }
-    } else if ( num2 != '' && num2 != '0' && displayField.textContent != '') {
-             if ( displayField.textContent == '0') {
+            sign = '';
             displayField.textContent = '';
+            displaySign.textContent = '';
+        } else if ( value == '=') {
+            if ((num1 != '' && num1 != '0') && (num2 != '' && num2 != '0')) {
+                displaySign.textContent = ''
+                num1 = operate(+num1, +num2, sign);
+                displayField.textContent = num1;
+                sign = '';
+                num2 = '';
+                temp = true;
+            }
+        }  else if ( num2 != '' && num2 != '0' && displayField.textContent != '') {
+                        displayField.textContent += value;
+                        num2 = displayField.textContent;
+        } else if ((( num1 != '' || num1 != '0' ) && sign != '') || (( num1 != '' || num1 != '0' ) && sign != '' &&  temp == true)) {
+        
+            if (displayField.textContent != '0.') {
+            displayField.textContent = '';  
             }
             displayField.textContent += value;
             num2 = displayField.textContent;
-    } else if ( ( num2 == '' || num2 == '0' ) && sign != '' ) {
-             displayField.textContent = ''; 
-             displayField.textContent += value;
-             num2 = displayField.textContent;
-    } else if ( value != '+/-' ) {
-            if ( displayField.textContent == '0') {
-                displayField.textContent = '';
-            }
-        displayField.textContent += value;
-        num1 = displayField.textContent;
-    }
+        } else if ( value != '+/-' && value != '+/-' ) {
+                if ( displayField.textContent == '0') {
+                    displayField.textContent = '';
+                }
+            displayField.textContent += value;
+            num1 = displayField.textContent;
+        }
 }
+
 
